a61c3d37179cac324239b6bfc0bafdaff6b6a4ed
Alicia Thoney <alicia.thoney@gmail.com>
2024-02-23 18:15:01 -0700

    fix pathing

--- src/middleware/api.jsx
+++ src/middleware/api.jsx
@@ -2,16 +2,14 @@ import { promises as fs } from 'fs';
 
 
 export async function getAllPosts() {
-    const file = await fs.readFile(process.cwd() + '/src/app/data.json', 'utf8');
+    const file = await fs.readFile('/src/app/data.json', 'utf8');
     console.log(process.cwd());
     const data = JSON.parse(file);
     return data.posts;
 }
 
 export async function getPost(id) {
-    const file = await fs.readFile(process.cwd() + '/src/app/data.json', 'utf8');
-    console.log(process.cwd());
-
+    const file = await fs.readFile('/src/app/data.json', 'utf8');
     const data = JSON.parse(file);
     let post = data.posts.filter(post => post.id == id);
     if (post.length === 0) {
