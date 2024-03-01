b94eee7d322238b3520a973d6a64a422b1053366
Alicia Thoney <alicia.thoney@gmail.com>
2024-03-01 13:34:47 -0700

    add scroll

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React from "react";
+import React, { Suspense } from "react";
 import Link from "next/link";
 import Image from 'next/image';
 import { roboto_mono, raleway } from "@/app/fonts";
@@ -7,10 +7,12 @@ import { notFound } from "next/navigation";
 import Button from "@/app/components/Button";
 import DOMPurify from "isomorphic-dompurify";
 import { posts } from "@/app/data";
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 
 export default async function Page({ params }) {
     const post = posts.find(post => post.id == params.id);
+    const { scrollYProgress } = useScroll();
+
     if (post === undefined) {
         return notFound();
     }
@@ -24,13 +26,14 @@ export default async function Page({ params }) {
                     className="mb-10"
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: 1, y: 0 }}
-                    transition={{ type: "easeIn"}}
+                    transition={{ type: "easeIn", delay: 0.5, duration: 1}}
 
                 >
                     {/* <h1 className={`text-3xl md:text-6xl font-bold ${roboto_mono.className} mt-5 md:mt-10`}>{post.title}</h1> */}
 
-
-                    <Image src={`/images/post${post.id}.png`} alt="Passport" width={500} height={300} className=' mx-auto mt-5 sm:mt-10' priority="true" />
+                    <Suspense fallback={<div className="w-100 bg-slate-500"></div>}>
+                        <Image src={`/images/post${post.id}.png`} alt="Passport" width={500} height={300} className=' mx-auto mt-5 sm:mt-10' priority="true" />
+                    </Suspense>
                     <div className="text-lg xl:text-xl text-start max-w-prose mx-auto">
                         <p className="mt-5 md:mt-10 text-start text-slate-500 text-lg xl:text-xl">{post.date}</p>
                         {post.paragraphs.map((paragraph, index) => {
@@ -43,8 +46,7 @@ export default async function Page({ params }) {
                 </motion.div>
             </div>
             <Button text="Return Home" link="/" />
-
-
+            <motion.div style={{ scaleX: scrollYProgress}} className="h-2 bg-primary fixed bottom-0 left-0 right-0"/>  
         </div >
     )
 }
